939e5349c731fa9c92e1cf1300fdb8c3509e6cb9
jocode2023 <olumideea@yahoo.com>
2023-02-21 19:59:33 -0800

    edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - prints _putchar
  *
@@ -7,17 +6,14 @@
  *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 char text[9] = "_putchar";
 int i = 0;
-
 for (i = 0; i < 8; i++)
 {
 _putchar(text[i]);
 }
 _putchar('\n');
-
 return (0);
 }
