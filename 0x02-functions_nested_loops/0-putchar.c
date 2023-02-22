5a6225d0158fbdaad57c5d24b991e482d2a58ff8
jocode2023 <olumideea@yahoo.com>
2023-02-22 12:56:57 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,23 @@
-#include "main.h"
+ #include "main.h"
+
 /**
- * main script
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
  * Return: 0 (Success)
- */ 
-int main (
-		{
-	_putchar("_");
-	-putchar("p");
-        -putchar("t");
-	-putchar("c");
-	-putchar("h");
-	-putchar("a");
-        _putchar("r");
-        _putchar("\n");
-	return (0);
+ */
+
+int main(void)
+{
+char text[9] = "_putchar";
+int i = 0;
+
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+
+return (0);
 }
