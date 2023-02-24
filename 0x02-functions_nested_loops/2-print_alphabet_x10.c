b767b61e7469f91b96997cf7b44e770f281751e6
jocode2023 <olumideea@yahoo.com>
2023-02-23 18:27:26 -0800

    Task debugging

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,16 +10,16 @@ void print_alphabet_x10(void)
 int round = 0;
 char letter = 'a';
 
-while (round < 10);
+while (round < 10); 
 {
-	letter = 'a';
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 
-	round++;
+round++;
 }
 }
