9d6d0f4dfd3d95b9f717c0b43938e625cfd03e40
jocode2023 <olumideea@yahoo.com>
2023-03-09 06:35:36 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,12 @@
 
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
+if (*s)
 {
-_putchar('\n');
-return;
-}
 _putchar(*s);
-_puts_recursion(++s);
+_puts_recursion(s+1);
+}
+else
+_putchar('\n');
+
 }
