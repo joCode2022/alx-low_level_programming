0d2f2be39ebe06b147823de00f1f264121baf4d3
jocode2023 <olumideea@yahoo.com>
2023-03-09 06:46:33 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,16 @@
  *
  * @s: string to be printed
  *
- * Return:0 sucess
+ * Return: 0
  */
 
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-_putchar(*s);
-_puts_recursion(s+1);
-}
-else
 _putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(++s);
 }
