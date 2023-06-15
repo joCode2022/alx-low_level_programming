83ae345736e8a4b7d97a1faabddf94a3d3bd9d53
joCode <105461756+joCode2022@users.noreply.github.com>
2023-06-15 03:14:43 +0100

    Update 0-create_array.c
    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,24 +1,34 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * *create_array - creates an array of chars
- * @size: size of array
- * @c: char to be filled in array
- * Return: pointer to the array
- */
+* create_array - create array of chars and initialize with a specific char
+* @size: size of array
+* @c: fill array values with this char
+* Return: pointer to array
+*/
 
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-char *str;
-str = malloc(sizeof(char) * size);
-if (size == 0 || !str)
-{
-return (NULL);
-}
-for (i = 0; i < size; i++)
-str[i] = c;
-return (str);
+	char *arr;
+	int i = 0;
+
+	if (size <= 0) /* validate size input*/
+
+	return (NULL);
+	arr = malloc(sizeof(char) * size); /* allocate memory */
+
+	if (arr == NULL) /* validate memory */
+
+	return (NULL);
+
+	while (i < (int)size) /* set array values to char c */
+	{
+		*(arr + i) = c;
+		i++;
+	}
+	*(arr + i) = '\0';
+
+	return (arr);
 }
