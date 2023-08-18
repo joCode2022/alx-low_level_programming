35fdf6e4511bedb0f4c2012bb6579c866f693d22
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 18:00:38 +0100

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,8 +4,8 @@
 /**
   * add_dnodeint - Adds a new node at the beginning
   * of a doubly linked list
-  * @head: The head of the doubly linked list
-  * @n: The number to place in the new node
+  * @head
+  * @n
   * Return: The new head of the doubly linked list
   */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
