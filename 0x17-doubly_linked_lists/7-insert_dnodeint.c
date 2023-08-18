508f9be62fbe900a02a0ae738b5b6f700df408b1
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 19:11:55 +0100

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -47,7 +47,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 /**
   * dlistint_len - Counts the number of elements in a doubly linked list
   * @h: The double linked list to count
-  *
   * Return: Number of elements in the doubly linked list
   */
 size_t dlistint_len(const dlistint_t *h)
