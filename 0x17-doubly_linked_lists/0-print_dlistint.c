e50daecc3022b5409e2cfc686199957b219ff99b
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 17:32:31 +0100

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int length = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		++length;
 		h = h->next;
 	}
+
 	return (length);
 }
