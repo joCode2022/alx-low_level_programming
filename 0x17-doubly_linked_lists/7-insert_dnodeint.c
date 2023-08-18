74bfee2ed68292b3b585276fa9a6f04d733b847c
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 19:06:35 +0100

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
   * insert_dnodeint_at_index - Inserts a new node at a given position
   * @h: The head of the doubly linked list
   * @idx: The index in which insert the new node
-  * @w: The number to insert in the new node
+  * @n: The number to insert in the new node
+  *
   * Return: The address of the new node, or NULL if it failed
   */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int w)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current = NULL, *new_node = NULL;
 	unsigned int iter_times = 0, length = 0;
@@ -17,20 +19,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int w)
 		return (NULL);
 
 	if (*h == NULL && idx == 0)
-		return (add_dnodeint(h, w));
+		return (add_dnodeint(h, n));
 
 	length = dlistint_len(*h);
 	if (idx == 0)
-		return (add_dnodeint(h, w));
+		return (add_dnodeint(h, n));
 	else if (length == idx)
-		return (add_dnodeint_end(h, w));
+		return (add_dnodeint_end(h, n));
 
 	current = *h;
 	while (current != NULL)
 	{
 		if (iter_times == idx)
 		{
-			new_node = create_node(w, current, current->prev);
+			new_node = create_node(n, current, current->prev);
 			current->prev = new_node;
 			current = new_node;
 			current->prev->next = new_node;
@@ -47,6 +49,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int w)
 /**
   * dlistint_len - Counts the number of elements in a doubly linked list
   * @h: The double linked list to count
+  *
   * Return: Number of elements in the doubly linked list
   */
 size_t dlistint_len(const dlistint_t *h)
@@ -64,12 +67,13 @@ size_t dlistint_len(const dlistint_t *h)
 
 /**
   * create_node - Create a new node with values
-  * @w: The number of the new node
+  * @n: The number of the new node
   * @next: The next node of the new node
   * @prev: The previous node of the new node
+  *
   * Return: The address of the new node created
   */
-dlistint_t *create_node(unsigned int w, void *next, void *prev)
+dlistint_t *create_node(unsigned int n, void *next, void *prev)
 {
 	dlistint_t *new_node = NULL;
 
@@ -77,7 +81,7 @@ dlistint_t *create_node(unsigned int w, void *next, void *prev)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->w = w;
+	new_node->n = n;
 	new_node->next = next;
 	new_node->prev = prev;
 	return (new_node);
