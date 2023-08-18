68aea982c684d71b347dab050f75fd1d8e179c6a
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 17:43:44 +0100

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,8 @@
 #include "lists.h"
 /**
  * print_dlistint - Prints all the elements of a double linked list
- * Return: The number of nodes of the double linked list
+ * @h: The double linked list to prints
+ *Return: The number of nodes of the double linked list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
