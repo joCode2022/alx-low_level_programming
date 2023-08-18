d39af7b9424eaefc1487e9d74cd06c0ae8aac117
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 18:17:36 +0100

    III-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
-  * add_dnodeint_end - Adds a new node at the end
+  * add_dnodeint_end - Addsanewnodeattheend
   * of a doubly linked list
   * @head: The head of the doubly linked list
   * @n: The number to place in the new node
-  *
-  * Return: The new head of the doubly linked list
+  *Return: The new head of the doubly linked list
   */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
