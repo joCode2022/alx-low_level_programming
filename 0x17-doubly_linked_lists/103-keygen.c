0ece2b3ee6f0d1604134cc7602f404d4940c06e3
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-18 19:22:19 +0100

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 /**
  * main - generate a key depending on a username for crackme5
  * @argc: number of arguments passed
  * @argv: arguments passed to main
- *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
