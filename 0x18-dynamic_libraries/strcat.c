bdefa74ef514b3da51b9a6706a9362849378e155
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-22 03:51:40 +0100

    strcat.c

--- 0x18-dynamic_libraries/strcat.c
+++ 0x18-dynamic_libraries/strcat.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
   * _strcat - Concatenates two strings
   * @dest: The destination string
   * @src: The source string
-  *
   * Return: A pointer to the resulting string dest
   */
 
