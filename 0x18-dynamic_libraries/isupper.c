241362d1f7690d5d1a42adf504e696808f9e8a77
joCode <105461756+joCode2022@users.noreply.github.com>
2023-08-22 03:54:53 +0100

    isupper.c

--- 0x18-dynamic_libraries/isupper.c
+++ 0x18-dynamic_libraries/isupper.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
   * _isupper - Check if a letter is upper
   * @x: The number to be checked
-  *
   * Return: 1 for upper letter or 0 for any else
   */
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
