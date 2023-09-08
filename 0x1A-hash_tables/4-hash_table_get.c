90f30c157820424035f6e78467b5ed99310a4ad9
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-08 13:46:41 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,17 +1,14 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_get - retrieves a value associated with a key
  * @ht: table to retrieve value from
  * @key: key to find value
- *
  * Return: value associated with key, or NULL if key cannot be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *tmp;
-
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 	    key == NULL || strlen(key) == 0)
 		return (NULL);
