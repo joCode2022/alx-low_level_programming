cfcea488d03dfb434c49ffe6a2a10fc2ca790d82
joCode <105461756+joCode2022@users.noreply.github.com>
2023-12-12 01:16:39 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,16 +11,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	int j;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < (int)size; i++)
+	for (j = 0; j < (int)size; j++)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return (i);
+		printf("Value checked array[%u] = [%d]\n", j, array[j]);
+		if (value == array[j])
+			return (j);
 	}
 	return (-1);
 }
